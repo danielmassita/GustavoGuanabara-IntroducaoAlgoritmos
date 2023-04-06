6c7681e1653f318a5e9c3fece9c5ca6a3c429abf
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-04-06 17:29:28 -0300

    Update and rename Aula_06.md to Aula_06.js

--- Aula_06.js
+++ Aula_06.js
@@ -1,4 +1,7 @@
-# Exercícios de Algoritmo Resolvidos - Curso de Algoritmos #06 - Gustavo Guanabara
-## https://youtu.be/v2nCgGSVCeE
+// Exercícios de Algoritmo Resolvidos - Curso de Algoritmos #06 - Gustavo Guanabara
+// https://youtu.be/v2nCgGSVCeE
 
+/*
 Vamos iniciar agora uma seqüência de exercícios, usando uma personagem fictícia pra resolver tanto no VisuAlg e no Scratch os resultados dos scripts. 
+
+*/
